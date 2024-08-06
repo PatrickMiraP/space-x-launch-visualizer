b040315d63a132b90f5f10212c1f55f680ad97f7
Tomas Neubauer <tomas@quix.ai>
2024-08-06 13:13:32 +0200

    Exceptional engineering

--- interpolation/main.py
+++ interpolation/main.py
@@ -1,28 +1,52 @@
 import os
 
 from quixstreams import Application, State
-
 # import the dotenv module to load environment variables from a file
 from dotenv import load_dotenv
 load_dotenv(override=False)
 
-app = Application()
+app = Application(consumer_group="interpolation-v1", auto_offset_reset="latest", use_changelog_topics=False)
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
+sdf.print()
 
-def interpolate(row: dict, state: State):
+def last_value(row: dict, state: State):
+    state_value = state.get("last_value", None)
+    
+    state.set("last_value", row)
+    
+    return state_value
 
-    # return the updated row so more processing can be done on it
-    return row
+sdf["last_value"] = sdf.apply(last_value, stateful=True)
 
-# apply the result of the count_names function to the row
-sdf = sdf.apply(interpolate, stateful=True)
+def interpolate(row: dict):
+    
+    if row["last_value"] is None:
+        return []
+    
+    delta_ms = (row["time"] - row["last_value"]["time"]) * 1000
+    steps = delta_ms / 100
+    
+    result = []
+    
+    for i in range(int(steps)):
+        new_row = {}
+        for key in row.keys():
+            if isinstance(row[key], (int, float)):
+                value_delta = row[key] - row["last_value"][key]
+                step = value_delta / steps
+                new_row[key] = row["last_value"][key] + (step * i)
+            if isinstance(row[key], (str)):
+                new_row[key] = row[key]
+        result.append(new_row)
+    
+    return result
 
-# print the row with this inline function
-sdf = sdf.update(lambda row: print(row))
+sdf = sdf.apply(interpolate, expand=True)
+
 
 # publish the updated row to the output topic
 sdf = sdf.to_topic(output_topic)
